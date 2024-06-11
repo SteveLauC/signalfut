fe63764499382cf4f91d3c8033b33e89c4877e76
Steve Lau <stevelauc@outlook.com>
2024-06-11 08:48:06 +0800

    fix build on macOS

--- src/pipe.rs
+++ src/pipe.rs
@@ -10,22 +10,20 @@ pub(crate) fn pipe() -> (OwnedFd, OwnedFd) {
 /// Return a pipe, with `O_CLOEXEC` set.
 #[cfg(target_os = "macos")]
 pub(crate) fn pipe() -> (OwnedFd, OwnedFd) {
-    use std::os::fd::AsRawFd;
-
     let (rx, tx) = nix::unistd::pipe().unwrap();
     let mut rx_flag = nix::fcntl::OFlag::from_bits(
-        nix::fcntl::fcntl(rx.as_raw_fd(), nix::fcntl::FcntlArg::F_GETFL).unwrap(),
+        nix::fcntl::fcntl(&rx, nix::fcntl::FcntlArg::F_GETFL).unwrap(),
     )
     .unwrap();
     rx_flag.insert(nix::fcntl::OFlag::O_CLOEXEC);
     let mut tx_flag = nix::fcntl::OFlag::from_bits(
-        nix::fcntl::fcntl(tx.as_raw_fd(), nix::fcntl::FcntlArg::F_GETFL).unwrap(),
+        nix::fcntl::fcntl(&tx, nix::fcntl::FcntlArg::F_GETFL).unwrap(),
     )
     .unwrap();
     tx_flag.insert(nix::fcntl::OFlag::O_CLOEXEC);
 
-    nix::fcntl::fcntl(rx.as_raw_fd(), nix::fcntl::FcntlArg::F_SETFL(rx_flag)).unwrap();
-    nix::fcntl::fcntl(tx.as_raw_fd(), nix::fcntl::FcntlArg::F_SETFL(tx_flag)).unwrap();
+    nix::fcntl::fcntl(&rx, nix::fcntl::FcntlArg::F_SETFL(rx_flag)).unwrap();
+    nix::fcntl::fcntl(&tx, nix::fcntl::FcntlArg::F_SETFL(tx_flag)).unwrap();
 
     (rx, tx)
 }
